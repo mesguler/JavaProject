2763e69ac5b5aa9b9e65d9f26df8bac71a836649
mesguler <mguler87@gmail.com>
2021-01-15 20:40:08 +0300

    Add files via upload

---
+++ grup42_subeler.sql
@@ -0,0 +1,46 @@
+--------------------------------------------------------
+--  File created - Perþembe-Ocak-14-2021   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table SUBELER
+--------------------------------------------------------
+
+  CREATE TABLE "SYSTEM"."SUBELER" 
+   (	"SUBENO" VARCHAR2(50 BYTE), 
+	"SUBEADI" VARCHAR2(50 BYTE)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+REM INSERTING into SYSTEM.SUBELER
+SET DEFINE OFF;
+Insert into SYSTEM.SUBELER (SUBENO,SUBEADI) values ('bid101','bilgi güvenliði koordinatörlüðü');
+Insert into SYSTEM.SUBELER (SUBENO,SUBEADI) values ('bid102','network koordinatörlüðü');
+Insert into SYSTEM.SUBELER (SUBENO,SUBEADI) values ('bid103','sistem koordinatörlüðü');
+Insert into SYSTEM.SUBELER (SUBENO,SUBEADI) values ('bid105','cografi bilgi sistemleri koordnatörlüðü');
+Insert into SYSTEM.SUBELER (SUBENO,SUBEADI) values ('bid104','donaným ve destek koordinatörlüðü');
+Insert into SYSTEM.SUBELER (SUBENO,SUBEADI) values ('bid106','idari ve mali iþler koordinatörlüðü');
+Insert into SYSTEM.SUBELER (SUBENO,SUBEADI) values ('bid107','yapay zeka koordinatörlüðü');
+Insert into SYSTEM.SUBELER (SUBENO,SUBEADI) values ('bid111','idari mali iþler');
+--------------------------------------------------------
+--  DDL for Index SYS_C007350
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "SYSTEM"."SYS_C007350" ON "SYSTEM"."SUBELER" ("SUBENO") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+--------------------------------------------------------
+--  Constraints for Table SUBELER
+--------------------------------------------------------
+
+  ALTER TABLE "SYSTEM"."SUBELER" ADD PRIMARY KEY ("SUBENO")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM"  ENABLE;
---
+++ grup42_users.sql
@@ -0,0 +1,23 @@
+--------------------------------------------------------
+--  File created - Perþembe-Ocak-14-2021   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table USERS
+--------------------------------------------------------
+
+  CREATE TABLE "SYSTEM"."USERS" 
+   (	"USERNAME" CHAR(50 BYTE), 
+	"PASSWORD" CHAR(50 BYTE), 
+	"ROL" NUMBER
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+REM INSERTING into SYSTEM.USERS
+SET DEFINE OFF;
+Insert into SYSTEM.USERS (USERNAME,PASSWORD,ROL) values ('admin                                             ','admin                                             ','0');
+Insert into SYSTEM.USERS (USERNAME,PASSWORD,ROL) values ('user                                              ','1234                                              ','1');
+Insert into SYSTEM.USERS (USERNAME,PASSWORD,ROL) values ('tarkan                                            ','1234                                              ','1');
+Insert into SYSTEM.USERS (USERNAME,PASSWORD,ROL) values ('tarkan1                                           ','1234                                              ','1');
